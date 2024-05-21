70834ab419eec92fb38cfa2eb2e8d5c4385f52a2
Enry6tz <enrique6tz@gmail.com>
2024-05-21 10:19:20 -0300

    solution nav

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/
-  });
-  
-  module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  });
+
 export default nextConfig;
